ddeddad0ec6afba36167d2c2e8c3d87e164f0fd3
kavitha-divum <kavitha@divum.in>
2021-07-19 12:13:28 +0530

    first commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+    const [isCheckout,setIsCheckout] = useState(false);
+    const [isSubmitting,setIsSubmitting] = useState(false);
+    const [didSubmitting,setDidSubmitting] = useState(false);
+    
     const cartCtx=useContext(CartContext);
 
     const totalAmount=`$${cartCtx.totalAmount.toFixed(2)}`;
@@ -15,6 +20,23 @@ const Cart = (props) => {
     };
     const cartItemAddHandler= (item) => {
         cartCtx.addItem({...item,amount:1});      
+    };
+    const orderHandler = () => {
+        setIsCheckout(true);
+    };
+
+    const submitOrderHandler=async (userData) => {
+        setIsSubmitting(true);
+        await fetch('https://react-http-5313b-default-rtdb.firebaseio.com/orders.json',{
+            method:'POST',
+            body:JSON.stringify({
+                user:userData,
+                orderedItems:cartCtx.items
+            })
+        });
+        setIsSubmitting(false);
+        setDidSubmitting(true);
+        cartCtx.clearCart();
     };
 
     const cartItems=( <ul  className={classes['cart-items']}>
@@ -27,18 +49,38 @@ const Cart = (props) => {
             onAdd={cartItemAddHandler.bind(null,item)}/>
         ))}</ul>
         );
-    return (
-        <Modal onClose={props.onClose}>
+        const modalActions = (
+            <div className={classes.actions}>
+            <button className={classes['button-alt']} onClick={props.onClose}>Close</button>
+            {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+        </div>
+        );
+
+        const cartModalContent = (<React.Fragment>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes['button-alt']} onClick={props.onClose}>Close</button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose}/>}
+            {!isCheckout && modalActions}
+        </React.Fragment>);
 
+        const isSubmittingModalContent = <p>Sending order data...</p>;
+        const didSubmitModalContent=(
+        <React.Fragment>
+             <p>Successfully sent the order!</p>
+             <div className={classes.actions}>
+            <button className={classes.button} onClick={props.onClose}>Close</button>
+            {/* {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>} */}
+        </div>   
+        </React.Fragment>);
+ 
+    return (
+        <Modal onClose={props.onClose}>
+            { !isSubmitting && !didSubmitting && cartModalContent}
+            { isSubmitting && isSubmittingModalContent}
+            { !isSubmitting && didSubmitting && didSubmitModalContent}
         </Modal>
     );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,35 +1,89 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 import Card from '../UI/Card';
-const DUMMY_MEALS = [
+// const DUMMY_MEALS = [
+//     {
+//       id: 'm1',
+//       name: 'Sushi',
+//       description: 'Finest fish and veggies',
+//       price: 22.99,
+//     },
+//     {
+//       id: 'm2',
+//       name: 'Schnitzel',
+//       description: 'A german specialty!',
+//       price: 16.5,
+//     },
+//     {
+//       id: 'm3',
+//       name: 'Barbecue Burger',
+//       description: 'American, raw, meaty',
+//       price: 12.99,
+//     },
+//     {
+//       id: 'm4',
+//       name: 'Green Bowl',
+//       description: 'Healthy...and green...',
+//       price: 18.99,
+//     },
+//   ];
+const AvailableMeals =() => {
+  const [meals,setMeals] =useState([]);
+  const [isLoading,setIsLoading]=useState(true);
+  const [httpError,setHttpError]=useState();
+
+    useEffect(()=>{
+      const fetchMeals = async () => {
+        const response= await fetch('https://react-http-5313b-default-rtdb.firebaseio.com/meals.json');
+
+        if(!response.ok){
+          throw new Error('Something went wrong!');
+        } 
+
+
+
+        const responseData=await response.json();
+
+        const loadedMeals=[];
+        for(const key in responseData)
+        {
+          loadedMeals.push({
+            id:key,
+            name:responseData[key].name,
+            description:responseData[key].description,
+            price:responseData[key].price
+            
+          });
+        }
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      };
+      
+        fetchMeals().catch(error => {
+          setIsLoading(false);
+          setHttpError(error.message);
+        });
+    },[]);
+
+    if(isLoading)
     {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
-const AvailableMeals =() =>{
-    const mealsList=DUMMY_MEALS.map(meal => 
+      return (
+        <sction className={classes.MealsLoading}>
+          <p>Loading...</p>
+        </sction>
+      );
+    }
+
+    if(httpError){
+      return (
+        <sction className={classes.MealsError}>
+          <p>{httpError}</p>
+        </sction>
+      );
+    }
+
+    const mealsList=meals.map(meal => 
     <MealItem 
         key={meal.id} 
         id={meal.id}
